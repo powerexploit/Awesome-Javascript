3f8d39668f5b75104b90a0f541c5794da2dced7a
Ankit Dobhal <dobhal.ankit@protonmail.com>
2019-10-18 08:08:52 +0530

    Create facebook.js

---
+++ Hack-Scripts/facebook.js
@@ -0,0 +1,6 @@
+/* fun script to accept friend request without using mouse */ 
+for( i = 0;i<document.getElementsByName("actions[accept]").length;i++)
+{
+    document.getElementsByName("actions[accept]")[i].click();
+    console.log("accept friend request")
+ }void(0);
