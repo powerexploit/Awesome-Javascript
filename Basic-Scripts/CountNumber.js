b0d8b5872a203dd88faa63b88631f86012594572
Wasitpol Jongmanasjaroean <41641716+RewiKaze@users.noreply.github.com>
2019-10-19 13:28:23 +0700

    Create CountNumber.js
    count number every second when run

---
+++ Basic-Scripts/CountNumber.js
@@ -0,0 +1,7 @@
+var num = 1;
+setInterval(() => {
+    //function code here
+    console.log(num);
+    num += 1;
+}, 1000); //  <= can change time here <1000 miliseconds = 1 second>, so every 1 second it will plus number
+          // it will go forever;
