b3800b61d96ddd59ded7827eea09925782637013
Leonardo Facundes <leonardo.fm00@gmail.com>
2019-10-20 11:38:30 -0300

    Add password generator to Hack Scripts

---
+++ Hack-Scripts/password_generator.js
@@ -0,0 +1,9 @@
+function generatePassword( length ){
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*-+?.';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    alert('Password: ' + result);
+}
