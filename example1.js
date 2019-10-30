08b5d8e9a49e0b18e19ee971746dad61830dc59e
anvisha16 <42858946+anvisha16@users.noreply.github.com>
2019-10-30 10:45:05 +0530

    Create example1.js

---
+++ example1.js
@@ -0,0 +1,26 @@
+<script> 
+  
+    // Calling BYTES_PER_ELEMENT on some typedArray 
+    a = Int8Array.BYTES_PER_ELEMENT; 
+    b = Uint8Array.BYTES_PER_ELEMENT; 
+    c = Uint8ClampedArray.BYTES_PER_ELEMENT; 
+    d = Int16Array.BYTES_PER_ELEMENT; 
+    e = Uint16Array.BYTES_PER_ELEMENT; 
+    f = Int32Array.BYTES_PER_ELEMENT; 
+    g = Uint32Array.BYTES_PER_ELEMENT; 
+    h = Float32Array.BYTES_PER_ELEMENT; 
+    i = Float64Array.BYTES_PER_ELEMENT; 
+  
+   // Printing the size in bytes of the 
+   // each element in the given typedArray. 
+   document.write(a + "<br>"); 
+   document.write(b + "<br>"); 
+   document.write(c + "<br>"); 
+   document.write(d + "<br>"); 
+   document.write(e + "<br>"); 
+   document.write(f + "<br>"); 
+   document.write(g + "<br>"); 
+   document.write(h + "<br>"); 
+   document.write(i); 
+  
+</script> 
